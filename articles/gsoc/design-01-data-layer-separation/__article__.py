717f3e510f1709e42abba35f955d8b202ced7dca
Jake Lishman <jake@binhbar.com>
2020-06-27 11:24:54 +0100

    Add design documents

---
+++ articles/gsoc/design-01-data-layer-separation/__article__.py
@@ -0,0 +1,6 @@
+{
+    "id": "d74ad2",
+    "title": "Design draft: data layer separation",
+    "date": "2020-06-12T16:35",
+    "tags": ["GSoC", "QuTiP", "python"],
+}
---
+++ articles/gsoc/design-02-data-structures/__article__.py
@@ -0,0 +1,6 @@
+{
+    "id": "11f082",
+    "title": "Design draft: data structure specifications",
+    "date": "2020-06-12T16:40",
+    "tags": ["GSoC", "QuTiP", "python"],
+}
