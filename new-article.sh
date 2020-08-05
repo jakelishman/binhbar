ee47d59b366906696ba1b764306672bfcbca351d
Jake Lishman <jake@binhbar.com>
2020-08-05 10:36:25 +0100

    Make new-article script add ID to store file

--- new-article.sh
+++ new-article.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+original=$(realpath "$(dirname "$0")")
+
 if [[ -z $1 ]]; then
     echo "Missing argument: storage directory." >&2
     exit 1
@@ -20,6 +22,9 @@ if [[ -z $title ]]; then
     exit 8
 fi
 
+
+## Generate an ID and drop the metadata in the file.
+
 id=$(date -Ins | sha256sum | head -c6)
 
 cat >"__article__.py" <<INFO_END
@@ -31,3 +36,21 @@ cat >"__article__.py" <<INFO_END
 }
 INFO_END
 touch "article.md"
+
+
+## Update store file with the path to the new article.
+
+cd "$original" || exit 16
+
+store_file=".hbar-store"
+
+if [[ ! -f "$store_file" ]]; then
+    echo -e "{\n}" >"$store_file"
+fi
+
+cp "$store_file" "${store_file}_backup"
+
+head -n-1 "$store_file" >"${store_file}_new"
+echo "    '$id': '$directory'," >>"${store_file}_new"
+echo "}" >>"${store_file}_new"
+mv "${store_file}_new" "${store_file}"
