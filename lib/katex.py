07068b63f7f216421af3990db0b2145896a72a48
Jake Lishman <jake@binhbar.com>
2020-11-22 00:19:25 +0000

    Insert KaTeX MathML nodes raw, not as escaped XML
    Previously, since the KaTeX MathML nodes were XML but not HTML, the
    Markdown parser was escaping the tags in them.  This didn't show up in
    regular webpages, but it destroyed the metadata around equations and
    made articles unreadable in reader mode on Firefox.
    
    This fixes it by adding this part of the KaTeX output to the raw HTML
    stash in python-markdown, and restoring it only after the HTML escaper
    has already run.

--- lib/katex.py
+++ lib/katex.py
@@ -22,6 +22,29 @@ def tohtml(latex, inline):
     return result.stdout.strip()
 
 
+def stash_mathml(stash, element):
+    """
+    Stash the MathML component of KaTeX output in pymarkdown's HTML stash,
+    replacing the stashed element with a temporary tag that is reinserted at
+    the XML-to-string conversion step.
+    """
+    subspan = list(element)[0]
+    if subspan.get('class') == 'katex-mathml':
+        mathml = subspan
+        parent = element
+    else:
+        parent = subspan
+        for mathml in parent:
+            if mathml.get('class') == 'katex-mathml':
+                break
+        else:
+            estr = etree.tostring(element, encoding="unicode")
+            raise ValueError("could not find the MathML span in: " + estr)
+    placeholder = stash.store(etree.tostring(mathml, encoding="unicode"))
+    parent.remove(mathml)
+    parent.text = placeholder + (parent.text or "")
+
+
 class KaTeXInline(markdown.inlinepatterns.InlineProcessor):
     def __init__(self, md):
         pattern = r'\$`(.*?)`\$'
@@ -29,14 +52,16 @@ class KaTeXInline(markdown.inlinepatterns.InlineProcessor):
 
     def handleMatch(self, m, data):
         el = etree.fromstring(tohtml(m.group(1), inline=True))
+        stash_mathml(self.md.htmlStash, el)
         return el, m.start(0), m.end(0)
 
 
 class KaTeXBlock(markdown.blockprocessors.BlockProcessor):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, md, *args, **kwargs):
         self._start = re.compile(r'^\s*\\\[')
         self._end = re.compile(r'\\\]\s*$')
-        super().__init__(*args, **kwargs)
+        self.md = md
+        super().__init__(md.parser, *args, **kwargs)
 
     def test(self, parent, block):
         return self._start.match(block)
@@ -58,6 +83,7 @@ class KaTeXBlock(markdown.blockprocessors.BlockProcessor):
             return False
         del blocks[:n_blocks]
         el = etree.fromstring(tohtml("\n".join(out), inline=False))
+        stash_mathml(self.md.htmlStash, el)
         parent.append(el)
         return True
 
@@ -70,5 +96,5 @@ class Extension(markdown.extensions.Extension):
         md.inlinePatterns.register(KaTeXInline(md), 'katex-inline', 200)
         # This priority is pretty much entirely arbitrary, so long as it's
         # higher than paragraph (10).
-        md.parser.blockprocessors.register(KaTeXBlock(md.parser), 'katex-block', 200)
+        md.parser.blockprocessors.register(KaTeXBlock(md), 'katex-block', 200)
         md.registerExtension(self)
